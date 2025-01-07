732afb0818b5eb437ca56715117f7f61787b11d3
Paloma Ferreira <palomaferreira71@gmail.com>
2025-01-07 14:01:23 -0300

    actua07/01

---
+++ class.js
@@ -0,0 +1,81 @@
+dinerocofla = prompt("cuanto dinero tiene cofla?");
+dinerorobrto = prompt("cuanto dinero tiene roberto?");
+dineropedro = prompt("cuanto dinero tiene pedro?");
+
+
+if (dinerocofla >= 0.6 && dinerocofla < 1 ) {
+    alert ("comprate helado de agua")
+}
+
+else if (dinerocofla >= 1 && dinerocofla < 1.6 ) {
+    alert ("cofla; comprate helado de crema")
+}
+else if (dinerocofla >= 1.6 && dinerocofla < 1.7 ) {
+    alert ("cofla; comprate helado de chocolate")
+}
+else if (dinerocofla >= 1.7 && dinerocofla < 1.8 ) {
+    alert ("cofla; comprate helado de frutilla")
+}
+else if (dinerocofla >= 1.8 && dinerocofla < 2.9 ) {
+    alert ("cofla;comprate helado de dulce de leche")
+}
+
+else if (dinerocofla >= 2.9) {
+    alert ("helado con confites o el pote de 1/4kg"); 
+}
+else {
+    alert ("lo sinto no te alcanza")
+}
+
+
+
+if (dinerorobrto >= 0.6 && dinerorobrto < 1 ) {
+    alert ("roberto;comprate helado de agua")
+}
+
+else if (dinerorobrto >= 1 && dinerorobrto < 1.6 ) {
+    alert ("roberto;comprate helado de crema")
+}
+else if (dinerorobrto >= 1.6 && dinerorobrto < 1.7 ) {
+    alert ("roberto;comprate helado de chocolate")
+}
+else if (dinerorobrto >= 1.7 && dinerorobrto < 1.8 ) {
+    alert ("roberto;comprate helado de frutilla")
+}
+else if (dinerorobrto >= 1.8 && dinerorobrto < 2.9 ) {
+    alert ("roberto;comprate helado de dulce de leche")
+}
+
+else if (dinerorobrto >= 2.9) {
+    alert ("helado con confites o el pote de 1/4kg"); 
+}
+else {
+    alert ("lo sinto no te alcanza")
+}
+
+
+if (dineropedro >= 0.6 && dineropedro < 1 ) {
+    alert ("pedro;comprate helado de agua")
+}
+
+else if (dineropedro>= 1 && dineropedro < 1.6 ) {
+    alert ("pedro;comprate helado de crema")
+}
+else if (dineropedro >= 1.6 && dineropedro < 1.7 ) {
+    alert ("pedro;comprate helado de chocolate")
+}
+else if (dineropedro >= 1.7 && dineropedro < 1.8 ) {
+    alert ("pedro;comprate helado de frutilla")
+}
+else if (dineropedro >= 1.8 && dineropedro < 2.9 ) {
+    alert ("pedro;comprate helado de dulce de leche")
+}
+
+else if (dineropedro >= 2.9) {
+    alert ("helado con confites o el pote de 1/4kg"); 
+}
+else {
+    alert ("lo sinto no te alcanza")
+}
+
+
